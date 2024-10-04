e59faef86be5b29dfec78dec4f3d9de1bd572f10
WarFade <96909243+WarFade@users.noreply.github.com>
2024-10-05 05:25:48 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,27 +1,34 @@
 const { Telegraf } = require('telegraf');
+const { checkMembership } = require('./membership');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+bot.use(checkMembership);
+
+bot.command('start', async (ctx) => {
+    await ctx.reply('Under Development');
+});
+
 bot.on('text', async (ctx) => {
-  await ctx.reply('Under Development.');
+    await ctx.reply('Under Development');
 });
 
 bot.catch((err, ctx) => {
-  console.error(`Error For ${ctx.updateType}`, err);
+    console.error(`Error For ${ctx.updateType}`, err);
 });
 
 module.exports = async (request, response) => {
-  try {
-    const { body, method } = request;
+    try {
+        const { body, method } = request;
 
-    if (method !== 'POST') {
-      response.status(200).json({ message: 'Bot Is Running!' });
-      return;
+        if (method !== 'POST') {
+            response.status(200).json({ message: 'Bot Is Running!' });
+            return;
+        }
+        await bot.handleUpdate(body);
+        response.status(200).json({ message: 'Success' });
+    } catch (error) {
+        console.error('Error In Webhook Handler:', error);
+        response.status(500).json({ error: 'Failed To Process Update' });
     }
-    await bot.handleUpdate(body);
-    response.status(200).json({ message: 'Success' });
-  } catch (error) {
-    console.error('Error In Webhook:', error);
-    response.status(500).json({ error: 'Failed To Process Update.' });
-  }
 };
